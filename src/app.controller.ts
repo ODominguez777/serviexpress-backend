accd8cf7fc134f68447b9f013e5a30f46eac347c
ODominguez777 <oscar@qordev.com>
2024-10-01 06:11:47 -0600

    Fuck chatgpt all my hommies use copilot

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,5 @@
 import { ApiTags, ApiResponse, ApiOperation } from '@nestjs/swagger';
-
 import { Controller, Get } from '@nestjs/common';
-
 import { AppService } from './app.service';
 
 @ApiTags('Users')
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { setupSwagger } from './config/swagger.config';
-
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app.module';
 
 async function bootstrap() {
