535fda8069ed8b30dffee676eee730ed8122a4d5
ODominguez777 <oscar@qordev.com>
2024-10-01 09:36:43 -0600

    app and main config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/ServiExpress'),
+    UsersModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,19 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/swagger.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configura el ValidationPipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true, // Elimina propiedades no definidas en el DTO
+      forbidNonWhitelisted: true, // Lanza un error si hay propiedades no definidas en el DTO
+    })
+  );
 
   //Swagger Config
   setupSwagger(app);
