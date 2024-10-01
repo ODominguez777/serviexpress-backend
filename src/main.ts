ea70a30e87a1ffcdaec8ea45162e9c7c906242ba
ODominguez777 <oscar@qordev.com>
2024-10-01 06:28:58 -0600

    Last epoch

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { setupSwagger } from './config/swagger.config';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+import { setupSwagger } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
