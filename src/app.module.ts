df61056f4571850078254532256d47b594bc0428
ODominguez777 <oscar@qordev.com>
2024-10-03 00:40:25 -0600

    use envvars instead literal connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,6 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/ServiExpress'),
-    UsersModule,
-  ],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URI), UsersModule],
 })
 export class AppModule {}
