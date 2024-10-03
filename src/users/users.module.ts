cc02fd74409472aabb63d0092cae4c2ffc341588
ODominguez777 <oscar@qordev.com>
2024-10-03 06:43:34 -0600

    import email module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,10 +4,12 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User, UserSchema } from './user.schema';
 import { ValidationService } from './validation/validation.service';
+import { EmailModule } from 'src/email/email.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    EmailModule,
   ],
   controllers: [UsersController],
   providers: [UsersService, ValidationService],
