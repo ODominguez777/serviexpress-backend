551b8d414e4a9fb1134c8b53e4ff041c9f84a225
ODominguez777 <oscar@qordev.com>
2024-10-03 01:39:16 -0600

    Encrypt password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,13 +3,18 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User, UserDocument } from './user.schema';
+import { hashPassword } from 'src/utils/password.util';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    const newUser = new this.userModel(createUserDto);
+    const hashedPassword = await hashPassword(createUserDto.password);
+    const newUser = new this.userModel({
+      ...createUserDto,
+      password: hashedPassword,
+    });
     return newUser.save();
   }
 
