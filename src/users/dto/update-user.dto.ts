64034cc1054cee0803668af6d48129a72fe13729
ODominguez777 <oscar@qordev.com>
2024-10-03 06:36:01 -0600

    create dto for updating users

---
+++ src/users/dto/update-user.dto.ts
@@ -0,0 +1,29 @@
+import { IsOptional, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UpdateUserDto {
+  @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
+  name?: string;
+
+  @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
+  lastname?: string;
+
+  @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
+  username?: string;
+
+  @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
+  phone?: string;
+
+  @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
+  profilePicUrl?: string;
+}
