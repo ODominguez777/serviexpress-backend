a11c74bcd0cb14edb48b1b1d57d69058183a9eda
ODominguez777 <oscar@qordev.com>
2024-10-03 00:20:44 -0600

    Import and use validation service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Post, Body, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { ValidationService } from './validation/validation.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './user.schema';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
@@ -7,11 +8,18 @@ import { ApiResponse, ApiTags } from '@nestjs/swagger';
 @ApiTags('Users')
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly validationService: ValidationService
+  ) {}
 
   @Post()
   @ApiResponse({ status: 201, description: 'User created.' })
   async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    await this.validationService.checkUserUniqueness(
+      createUserDto.username,
+      createUserDto.email
+    );
     return this.usersService.create(createUserDto);
   }
 
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,12 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User, UserSchema } from './user.schema';
+import { ValidationService } from './validation/validation.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, ValidationService],
 })
 export class UsersModule {}
