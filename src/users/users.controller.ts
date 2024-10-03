b050c411b8b2edac0805f5ee1d42068cd5efca46
ODominguez777 <oscar@qordev.com>
2024-10-03 06:50:10 -0600

    Create endpoints and services for: activate account, find by email or username, get user by id, update user, change password

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,22 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Query,
+  Put,
+  Patch,
+  Res,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ValidationService } from './validation/validation.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './user.schema';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UpdatePasswordDto } from './dto/update-password.dto';
+import { Response } from 'express';
 
 @ApiTags('Users')
 @Controller('users')
@@ -14,7 +27,8 @@ export class UsersController {
   ) {}
 
   @Post()
-  @ApiResponse({ status: 201, description: 'User created.' })
+  @ApiOperation({ summary: 'Create a new user' })
+  @ApiResponse({ status: 201, description: 'User Created.' })
   async create(@Body() createUserDto: CreateUserDto): Promise<User> {
     await this.validationService.checkUserUniqueness(
       createUserDto.username,
@@ -24,8 +38,60 @@ export class UsersController {
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all users' })
   @ApiResponse({ status: 200, description: 'Get all users.' })
   async findAll(): Promise<User[]> {
-    return this.usersService.findAll(); // Obtiene todos los usuarios
+    return this.usersService.findAll();
+  }
+
+  @Get('activate-account')
+  async activateAccount(
+    @Query('code') code: string,
+    @Res() res: Response
+  ): Promise<void> {
+    console.log('Acasta');
+    try {
+      await this.usersService.activateAccount(code);
+      res.redirect('/public/activation-success.html');
+    } catch (error) {
+      console.error(error);
+      res.redirect('/public/activation-failed.html');
+    }
+  }
+
+  @Get('find')
+  @ApiOperation({ summary: 'Get user by email or username' })
+  @ApiResponse({ status: 200, description: 'Get user by email or username.' })
+  async findUserByUsernameOrEmail(
+    @Query('identifier') identifier: string
+  ): Promise<Partial<User>> {
+    return this.usersService.findUserByUsernameOrEmail(identifier);
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Get user by ID' })
+  @ApiResponse({ status: 200, description: 'Get user by Id.' })
+  async findUserById(@Param('id') id: string): Promise<Partial<User>> {
+    return this.usersService.findUserById(id); // Obtiene todos los usuarios
+  }
+
+  @Put(':id')
+  @ApiOperation({ summary: 'Update user data' })
+  @ApiResponse({ status: 200, description: 'User updated successfully.' })
+  async updateUser(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto
+  ): Promise<Partial<User>> {
+    return this.usersService.updateUser(id, updateUserDto);
+  }
+
+  @Patch(':id/password')
+  @ApiOperation({ summary: 'Change Password' })
+  @ApiResponse({ status: 200, description: 'Password updated successfully.' })
+  async updatePassword(
+    @Param('id') id: string,
+    @Body() updatePasswordDto: UpdatePasswordDto
+  ): Promise<void> {
+    return this.usersService.updatePassword(id, updatePasswordDto);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,24 +1,146 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User, UserDocument } from './user.schema';
-import { hashPassword } from 'src/utils/password.util';
+import { comparePasswords, hashPassword } from 'src/utils/password.util';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UpdatePasswordDto } from './dto/update-password.dto';
+import { EmailService } from 'src/email/email.service';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    private readonly emailService: EmailService
+  ) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     const hashedPassword = await hashPassword(createUserDto.password);
+    const activationCode = uuidv4();
     const newUser = new this.userModel({
       ...createUserDto,
       password: hashedPassword,
+      activationCode,
     });
-    return newUser.save();
+    const activationUrl = `${process.env.BACKEND_URL}/users/activate-account?code=${activationCode}`;
+    await this.emailService.sendActivationEmail(
+      createUserDto.email,
+      activationUrl
+    );
+    const user = await newUser.save();
+    return user;
+  }
+  async activateAccount(activationCode: string): Promise<void> {
+    console.log('Codigo', activationCode);
+    const user = await this.userModel.findOne({ activationCode }).exec();
+
+    if (!user) {
+      throw new NotFoundException('Invalid activation code.');
+    }
+
+    if (user.isActive) {
+      throw new BadRequestException('Account is already activated.');
+    }
+
+    user.isActive = true;
+    user.activationCode = null;
+    try {
+      await user.save();
+      console.log('Usuario guardado exitosamente');
+    } catch (error) {
+      console.error('Error al guardar el usuario:', error);
+    }
   }
 
   async findAll(): Promise<User[]> {
     return this.userModel.find().exec(); // Encuentra todos los usuarios
+  }
+
+  async findUserById(id: string): Promise<Partial<User>> {
+    if (!Types.ObjectId.isValid(id)) {
+      throw new BadRequestException(`Invalid ID format: ${id}`);
+    }
+    const user = await this.userModel
+      .findById(id)
+      .select('name lastname email username profilePicUrl phone -_id ')
+      .exec();
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+    return user;
+  }
+
+  async findUserByUsernameOrEmail(identifier: string): Promise<Partial<User>> {
+    const user = await this.userModel
+      .findOne({
+        $or: [{ username: identifier }, { email: identifier }],
+      })
+      .select('name lastname email username profilePicUrl phone -_id')
+      .exec();
+    console.log('usuario', user);
+    if (!user) {
+      throw new NotFoundException(`User with ID ${identifier} not found`);
+    }
+    return user;
+  }
+
+  async updateUser(
+    id: string,
+    updateUserDto: UpdateUserDto
+  ): Promise<Partial<User>> {
+    if (!Types.ObjectId.isValid(id)) {
+      throw new BadRequestException(`Invalid ID format: ${id}`);
+    }
+    const updatedUser = await this.userModel
+      .findByIdAndUpdate(id, { $set: updateUserDto }, { new: true })
+      .select('name lastname email username profilePicUrl phone -_id')
+      .exec();
+    if (!updatedUser) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+    return updatedUser;
+  }
+
+  async updatePassword(
+    id: string,
+    updatePasswordDto: UpdatePasswordDto
+  ): Promise<void> {
+    if (!Types.ObjectId.isValid(id)) {
+      throw new BadRequestException(`Invalid ID format: ${id}`);
+    }
+
+    if (updatePasswordDto.currentPassword === updatePasswordDto.password) {
+      throw new BadRequestException('You must choose different passwords');
+    }
+    const user = await this.userModel.findById(id).exec();
+
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+
+    const isMatch = await comparePasswords(
+      updatePasswordDto.currentPassword,
+      user.password
+    );
+
+    if (!isMatch) {
+      throw new BadRequestException('The password is incorrect');
+    }
+
+    const hashedPassword = await hashPassword(updatePasswordDto.password);
+
+    await this.userModel
+      .findByIdAndUpdate(
+        id,
+        { $set: { password: hashedPassword } },
+        { new: true }
+      )
+      .exec();
   }
 }
