e9d374f7989e4311d0ab9f9e04d9986abf0900b2
ODominguez777 <oscar@qordev.com>
2024-10-05 07:31:29 -0600

    /feat create validateUser function

--- src/users/auth/auth.service.ts
+++ src/users/auth/auth.service.ts
@@ -1,4 +1,23 @@
+/* eslint-disable */
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users.service';
+import { comparePasswords } from 'src/utils/password.util';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService
+  ) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.userService.findUserByUsernameOrEmail(username);
+    if (user && (await comparePasswords(pass, user.password))) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+}
