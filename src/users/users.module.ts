307da03dcf6ac5017bb55aeb4ad3067c12edaf00
ODominguez777 <oscar@qordev.com>
2024-10-05 06:58:47 -0600

    feat/ Add Auth module and service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,11 +5,13 @@ import { UsersService } from './users.service';
 import { User, UserSchema } from './user.schema';
 import { ValidationService } from './validation/validation.service';
 import { EmailModule } from 'src/email/email.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     EmailModule,
+    AuthModule,
   ],
   controllers: [UsersController],
   providers: [UsersService, ValidationService],
