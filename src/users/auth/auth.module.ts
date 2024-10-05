402ca4f79324cb7d72e1740f5a577313f69ccefb
ODominguez777 <oscar@qordev.com>
2024-10-05 08:12:48 -0600

    feat/ config providers, imports and exports

--- src/users/auth/auth.module.ts
+++ src/users/auth/auth.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../users.module';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  providers: [AuthService],
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRATION || '1h' },
+    }),
+    UsersModule,
+  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
